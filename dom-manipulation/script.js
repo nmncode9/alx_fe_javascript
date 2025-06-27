69bbc2b73ea9abf01c0c7aa8f4d5401c7a119a1e
numan <197640394+nmncode9@users.noreply.github.com>
2025-06-28 00:55:27 +0100

    Implement persistence with localStorage

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,20 +4,20 @@ const generateButton = document.getElementById("newQuote");
 const createQuote = document.querySelector("#newQuoteText");
 const createCategory = document.querySelector("#newQuoteCategory");
 
-let quoteList = [
-    {
-        text: "People who think they know everything are a great annoyance to those of us who do.",
-        category: "Funny"
-    },
-    {
-        text: "I think music in itself is healing. It's an explosive expression of humanity. It's something we are all touched by. No matter what culture we're from, everyone loves music.",
-        category: "Music"
-    },
-    {
-        text: "Music washes away from the soul the dust of everyday life.",
-        category: "Music"
-    },
-];
+let quoteList = JSON.parse(localStorage.getItem("quoteList")) || [
+            {
+                text: "People who think they know everything are a great annoyance to those of us who do.",
+                category: "Funny"
+            },
+            {
+                text: "I think music in itself is healing. It's an explosive expression of humanity. It's something we are all touched by. No matter what culture we're from, everyone loves music.",
+                category: "Music"
+            },
+            {
+                text: "Music washes away from the soul the dust of everyday life.",
+                category: "Music"
+            },
+        ];
 
 function random(n) {
     return Math.floor((Math.random() * n));
@@ -44,6 +44,7 @@ function addQuote() {
         alert("Please ensure you have filled all the fields.");
     } else {
         quoteList.push({text: tempQuote, category: tempCat})
+        localStorage.setItem("quoteList", JSON.stringify(quoteList));
         createQuote.value = "";
         createCategory.value = "";
     }
